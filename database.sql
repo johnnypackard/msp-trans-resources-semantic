d5e253a895493bf73ef91550a320ec2228025194
Johnny Packard <packard81@me.com>
2018-07-21 15:56:14 -0500

    updating database

--- database.sql
+++ database.sql
@@ -1,12 +1,6 @@
-CREATE TABLE person (
-	id SERIAL PRIMARY KEY,
-	username VARCHAR (80) UNIQUE NOT NULL,
-	password VARCHAR (1000) NOT NULL
-);
-
 CREATE TABLE registered_users (
 	id SERIAL PRIMARY KEY,
-	person_id INT REFERENCES "person",
+	person_id INT REFERENCES "businesses",
 	profile_image VARCHAR (1000),
 	gender VARCHAR (100),
 	pronouns VARCHAR (100),
@@ -21,18 +15,26 @@ CREATE TABLE businesses (
 	gender VARCHAR (100),
 	race VARCHAR (100),
 	language VARCHAR (100),
-	image_url VARCHAR (2083)
+	image_url VARCHAR (2083),
+	username VARCHAR (80) UNIQUE NOT NULL,
+    password VARCHAR (1000) NOT NULL,
+	biz_id INT REFERENCES "resources",
 );
 
-CREATE TABLE reviews (
-	id SERIAL PRIMARY KEY,
-	rating INT,
-	comment VARCHAR (1000),
-	biz_id INT REFERENCES "businesses"
+CREATE TABLE resources (
+    id SERIAL PRIMARY KEY,
+    biz_name VARCHAR (1000),
+    contact_name VARCHAR (100),
+    address VARCHAR (500),
+    gender VARCHAR (100),
+    race VARCHAR (100),
+    language VARCHAR (100),
+    image_url VARCHAR (2083),
+	biz_notes VARCHAR (2083),
+	resources_id INT REFERENCES "search",
 );
-
-CREATE TABLE favorites (
+ 
+CREATE TABLE search (
 	id SERIAL PRIMARY KEY,
-	user_id INT REFERENCES "registered_users",
-	biz_id INT REFERENCES "businesses"
+	search_id INT REFERENCES "businesses"
 );
